d9abab0d0fdcf51f9fbf1807b154e65f8a9101aa
sportalski <shace209@o2.pl>
2016-12-30 17:56:24 +0100

    #1 projekt

---
+++ MVCProj/MVCProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCProj.Startup))]
+namespace MVCProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
