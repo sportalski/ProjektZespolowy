ed5248f885609380f05f1b9db1ae9de5352f67ad
sportalski <shace209@o2.pl>
2017-01-04 18:30:12 +0100

    #18

--- MVCProj/MVCProj.Tests/Properties/AssemblyInfo.cs
+++ MVCProj/MVCProj.Tests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00763bb4-60f3-41c5-85a2-760191fb3fc5")]
+[assembly: Guid("1666f3ba-b3b7-4353-9b8d-026383f1d74d")]
 
 // Version information for an assembly consists of the following four values:
 //
