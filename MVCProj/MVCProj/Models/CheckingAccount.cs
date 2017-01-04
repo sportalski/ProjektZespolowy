7454cfd850a127b9ac24aa26f830e0fa2107e720
sportalski <shace209@o2.pl>
2017-01-04 17:10:53 +0100

    #7

---
+++ MVCProj/MVCProj/Models/CheckingAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCProj.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(10)]
+        [Column(TypeName="varchar")]
+        [RegularExpression(@"\d{6,10}", ErrorMessage="Numer konta # ma zawierac od 6 do 10 znakow" )]
+        [Display(Name="Numer konta #")]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Imie")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        public string Name { get { return String.Format("{0} {1}", this.FirstName, this.LastName); } }
+        
+        [Display(Name = "Stan konta")]
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
