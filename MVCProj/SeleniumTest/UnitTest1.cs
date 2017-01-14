b6811d390b3ab8e8831c5d51556e5f1d0624dee9
mkrakowiak <mkrakowiak@sigma.ug.edu.pl>
2017-01-14 14:52:21 +0100

    Test logowanie błędny mail

--- MVCProj/SeleniumTest/UnitTest1.cs
+++ MVCProj/SeleniumTest/UnitTest1.cs
@@ -75,5 +75,15 @@ namespace SeleniumTest
             driverGC.FindElement(By.XPath("/html[@class=' js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths']/body/div[@class='container body-content']/div[@class='row'][4]/div[@class='col-md-12 third-section']/div[@class='row'][5]/div[@class='form-section']/form/div[@class='form-group text-center']/div[@class='col-md-12']/input[@class='btn btn-default']")).Click();
             Assert.IsTrue(driverGC.PageSource.Contains("Wartość w polu Twój adres email nie jest prawidłowym adresem e-mail."));
         }
+        [TestMethod]
+        public void TestLogowanieBlednyMail()
+        {
+            driverGC.Navigate().GoToUrl("http://localhost:56431/");
+            driverGC.FindElement(By.LinkText("Log in")).Click();
+            driverGC.FindElement(By.Name("Email")).SendKeys("logowanie");
+            driverGC.FindElement(By.Name("Password")).SendKeys("1Q2w3e4r%");
+            driverGC.FindElement(By.XPath("/html[@class=' js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths']/body/div[@class='container body-content']/div[@class='row']/div[@class='col-md-8']/section[@id='loginForm']/form[@class='form-horizontal']/div[@class='form-group'][4]/div[@class='col-md-offset-2 col-md-10']/input[@class='btn btn-default']")).Click();
+            Assert.IsTrue(driverGC.PageSource.Contains("Wartość w polu Email nie jest prawidłowym adresem e-mail."));
+        }
     }
 }
