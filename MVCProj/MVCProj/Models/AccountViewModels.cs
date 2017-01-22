7f16d1e68003097030d63dcbd37759c9dc7ad101
Monika <napieraczmonika@gmail.com>
2017-01-22 17:21:39 +0100

    Edycja widoku 'zmiany hasła' #30

--- MVCProj/MVCProj/Models/AccountViewModels.cs
+++ MVCProj/MVCProj/Models/AccountViewModels.cs
@@ -20,18 +20,18 @@ namespace MVCProj.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Aktualne hasło")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} musi mieć długość conajmniej {2} znaków.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nowe hasło")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Powtórzone nowe hasło")]
+        [Compare("NewPassword", ErrorMessage = "Nowe hasło i powtórzone nowe hasło muszą być takie same.")]
         public string ConfirmPassword { get; set; }
     }
 
