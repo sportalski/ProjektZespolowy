3ec3dd81e2250f6cc23281c92fadcfed8c07ad7a
Monika <napieraczmonika@gmail.com>
2017-01-28 22:25:00 +0100

    Dodanie modelu do formularza przelewu, połaczenie go z widokiem #32

---
+++ MVCProj/MVCProj/Models/TransferModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCProj.Models
+{
+    public class TransferModel
+    {
+
+        [Display(Name = "Rachunek odbiorcy")]
+        public string ReceiverAccount { get; set; }
+
+        [Display(Name = "Tytuł przelewu")]
+        public string Title { get; set; }
+
+        [Display(Name = "Imię i nazwisko odbiorcy")]
+        public string ReceiverName { get; set; }
+
+        [Display(Name = "Adres odbiorcy")]
+        public string ReceiverAddress { get; set; }
+
+        [Display(Name = "Imię i nazwisko nadawcy")]
+        public string UserName { get; set; }
+
+        [Display(Name="Rachunek nadawcy")]
+        public string AccountNumber { get; set; }
+
+        [Display(Name = "Dostępne środki")]
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+
+        [Display(Name = "Kwota przelewu")]
+        public string Amount { get; set; }
+
+    }
+}
